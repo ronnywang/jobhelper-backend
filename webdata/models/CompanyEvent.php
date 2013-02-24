f047b5455aebfd05e4a0d40927d68906250a7659
Ronny Wang <ronnywang@gmail.com>
2013-02-24 15:15:14 +0800

    修改 table schema

--- webdata/models/CompanyEvent.php
+++ webdata/models/CompanyEvent.php
@@ -10,9 +10,9 @@ class CompanyEvent extends Pix_Table
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['company_no'] = array('type' => 'int');
         $this->_columns['time'] = array('type' => 'int');
-        // 資料來源: 1-台北市勞工局
-        $this->_columns['type'] = array('type' => 'int');
-        $this->_columns['data'] = array('type' => 'text');
+        $this->_columns['from'] = array('type' => 'varchar', 'size' => 255);
+        $this->_columns['snapshot'] = array('type' => 'varchar', 'size' => 255);
+        $this->_columns['message'] = array('type' => 'text');
 
         $this->addIndex('company', array('company_no', 'time'));
     }
