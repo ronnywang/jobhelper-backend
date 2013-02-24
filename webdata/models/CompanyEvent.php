4429e935d6573fd502e925765d896e610cbcabce
Ronny Wang <ronnywang@gmail.com>
2013-02-24 14:33:26 +0800

    加上 type

--- webdata/models/CompanyEvent.php
+++ webdata/models/CompanyEvent.php
@@ -10,6 +10,8 @@ class CompanyEvent extends Pix_Table
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['company_no'] = array('type' => 'int');
         $this->_columns['time'] = array('type' => 'int');
+        // 資料來源: 1-台北市勞工局
+        $this->_columns['type'] = array('type' => 'int');
         $this->_columns['data'] = array('type' => 'text');
 
         $this->addIndex('company', array('company_no', 'time'));
