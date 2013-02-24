5a6ea768a2e596b17b711e44ec1c933f98011c3c
Ronny Wang <ronnywang@gmail.com>
2013-02-24 11:51:57 +0800

    加上抓公司資訊

--- webdata/models/CompanyService.php
+++ webdata/models/CompanyService.php
@@ -2,7 +2,7 @@
 
 class CompanyService
 {
-    protected static function http($url)
+    public static function http($url)
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
@@ -27,6 +27,31 @@ class CompanyService
             return $result->data[0]->{'統一編號'};
         }
 
+        $id = null;
+        // 有多個的話，看看是不是只有一個 prefix
+        foreach ($result->data as $data) {
+            if ($data->{'公司名稱'}) {
+                if (0 !== strpos($data->{'公司名稱'}, $name)) {
+                    continue;
+                }
+            } elseif ($data->{'商業名稱'}) {
+                if (0 !== strpos($data->{'商業名稱'}, $name)) {
+                    continue;
+                }
+            } else {
+                // 找不到名稱，就直接找不到
+                return false;
+            }
+            // 如果已經有找到過了，那就不要了
+            if (!is_null($id)) {
+                return false;
+            }
+            $id = $data->{'統一編號'};
+        }
+
+        if (!is_null($id)) {
+            return $id;
+        }
         return false;
     }
 }
