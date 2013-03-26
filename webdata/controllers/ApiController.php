a8ae7352885d13e77a164a06278d0b7a8be5e11d
Ronny Wang <ronnywang@gmail.com>
2013-03-26 17:32:06 +0800

    加上 package_time

--- webdata/controllers/ApiController.php
+++ webdata/controllers/ApiController.php
@@ -16,6 +16,7 @@ class ApiController extends Pix_Controller
             $package_info->name = $package->name;
             $package_info->url = 'http://' . $_SERVER['SERVER_NAME'] . '/package/show/' . $package->package_id;
             $package_info->updated_at = intval($package->updated_at);
+            $package_info->package_time = intval($package->package_time);
             $packages[] = $package_info;
         }
         $ret->packages = $packages;
