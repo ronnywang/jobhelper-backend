cd0853e4d0e8b7f11171d301d9dd1ae1ae321ea2
Ronny Wang <ronnywang@gmail.com>
2013-03-26 15:13:52 +0800

    add api/getpackages

---
+++ webdata/controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+class ApiController extends Pix_Controller
+{
+    public function init()
+    {
+    }
+
+    public function getpackagesAction()
+    {
+        $ret = new StdClass;
+        $packages = array();
+        foreach (Package::search(1) as $package) {
+            $package_info = new StdClass;
+            $package_info->id = intval($package->package_id);
+            $package_info->name = $package->name;
+            $package_info->url = 'http://' . $_SERVER['SERVER_NAME'] . '/package/show/' . $package->package_id;
+            $package_info->updated_at = intval($package->updated_at);
+            $packages[] = $package_info;
+        }
+        $ret->packages = $packages;
+
+        return $this->json($ret);
+    }
+}
