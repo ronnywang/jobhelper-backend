b232584b9d491de7e01eab9eac37f7e058ef36d8
Ronny Wang <ronnywang@gmail.com>
2013-03-28 15:31:30 +0800

    拿掉 user_id 的 auto_increment

--- webdata/models/TeamMember.php
+++ webdata/models/TeamMember.php
@@ -8,7 +8,7 @@ class TeamMember extends Pix_Table
 
         $this->_primary = array('user_id', 'team_id');
 
-        $this->_columns['user_id'] = array('type' => 'int', 'auto_increment' => true);
+        $this->_columns['user_id'] = array('type' => 'int');
         $this->_columns['team_id'] = array('type' => 'int');
 
         $this->_relations['team'] = array('rel' => 'has_one', 'type' => 'Team', 'foreign_key' => 'team_id');
