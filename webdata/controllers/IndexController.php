445e3dec351de28828ac6ca29dfd1541c8a60547
Ronny Wang <ronnywang@gmail.com>
2013-04-01 17:35:31 +0800

    拿掉以經不用的 /index/info

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -15,27 +15,6 @@ class IndexController extends Pix_Controller
         $this->view->user = ($user_id = Pix_Session::get('user_id')) ? User::find(intval($user_id)) : null;
     }
 
-    public function infoAction()
-    {
-        if (!is_scalar($_GET['info'])) {
-            return $this->errorJson('wrong info');
-        }
-        if (!$info = @json_decode($_GET['info'])) {
-            return $this->errorJson('wrong info');
-        }
-
-        $ret = new StdClass;
-        $ret->error = false;
-        $id = Site::findCompanyByInfo($info);
-        if ($id) {
-            $ret->body = $this->view->partial('/index/company.phtml', array('no' => $id, 'name' => $info->name));
-        } else {
-            $ret->body = '找不到這家公司資料';
-        }
-
-        return $this->json($ret);
-    }
-
     public function showAction()
     {
         list(, /*index*/, /*show*/, $id) = explode('/', $this->getURI());
