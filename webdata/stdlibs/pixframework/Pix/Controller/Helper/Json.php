9ee0b93b4dc5afba3402aa7ac84d1dc7db539375
Ronny Wang <ronnywang@gmail.com>
2013-07-31 10:04:46 +0800

    add gzip encoding

--- webdata/stdlibs/pixframework/Pix/Controller/Helper/Json.php
+++ webdata/stdlibs/pixframework/Pix/Controller/Helper/Json.php
@@ -22,7 +22,7 @@ class Pix_Controller_Helper_Json extends Pix_Helper
     public function json($controller, $obj)
     {
         header('Content-Type: application/json');
-        echo @json_encode($obj);
+        $this->encodingFilter(@json_encode($obj));
         return $controller->noview();
     }
 
@@ -32,7 +32,35 @@ class Pix_Controller_Helper_Json extends Pix_Helper
         if (!preg_match('/^[a-zA-Z0-9_]+$/', strval($callback))) {
             return $controller->json($obj);
         }
-        echo $callback . '(' . @json_encode($obj) . ')';
+        $this->encodingFilter($callback . '(' . @json_encode($obj) . ')');
         return $controller->noview();
+    }
+
+    public function encodingFilter($content)
+    {
+        $HTTP_ACCEPT_ENCODING = $_SERVER["HTTP_ACCEPT_ENCODING"];
+        if( headers_sent() )
+            $encoding = false;
+        else if( strpos($HTTP_ACCEPT_ENCODING, 'x-gzip') !== false )
+            $encoding = 'x-gzip';
+        else if( strpos($HTTP_ACCEPT_ENCODING,'gzip') !== false )
+            $encoding = 'gzip';
+        else
+            $encoding = false;
+
+        if( $encoding ) {
+            $_temp1 = strlen($content);
+            if ($_temp1 < 2048)    // no need to waste resources in compressing very little data
+                print($content);
+            else {
+                header('Content-Encoding: '.$encoding);
+                print("\x1f\x8b\x08\x00\x00\x00\x00\x00");
+                $content = gzcompress($content, 9);
+                $content = substr($content, 0, $_temp1);
+                print($content);
+            }
+        } else {
+            echo $content;
+        }
     }
 }
