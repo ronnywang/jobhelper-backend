7001f8cd735f52e5f5e3294db220981271af04b5
Ronny Wang <ronnywang@gmail.com>
2015-05-06 10:47:26 +0800

    force ssl

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ include(__DIR__ . '/webdata/init.inc.php');
 if (!getenv('SESSION_SECRET')) {
     die("need SESSION_SECRET");
 }
-Pix_Session::setAdapter('cookie', array('secret' => getenv('SESSION_SECRET')));
+Pix_Session::setAdapter('cookie', array(
+    'secret' => getenv('SESSION_SECRET'),
+    'secure' => true,
+));
 Pix_Controller::addCommonHelpers();
 Pix_Controller::dispatch(__DIR__ . '/webdata/');
