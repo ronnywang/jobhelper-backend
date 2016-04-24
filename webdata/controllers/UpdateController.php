8590f7565575a8a1afc4dff5d538bb38e2654880
Ronny Wang <ronnywang@gmail.com>
2016-04-24 16:38:52 +0800

    加上 /update/checkurl API 檢查網址是否是合法的資料

--- webdata/controllers/UpdateController.php
+++ webdata/controllers/UpdateController.php
@@ -13,4 +13,19 @@ class UpdateController extends Pix_Controller
         );
     }
 
+    public function checkurlAction()
+    {
+        try {
+            $records = ImportLib::get_records_from_url($_REQUEST['url']);
+        } catch (Exception $e) {
+            return $this->json(array(
+                'error' => true,
+                'message' => $e->getMessage(),
+            ));
+        }
+        return $this->json(array(
+            'error' => false,
+            'records' => $records,
+        ));
+    }
 }
